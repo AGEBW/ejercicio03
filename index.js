38a46eb46515df68d7c0be09b706c9b03ecb20e4
AGEBW <a.d.river@hotmail.com>
2019-02-26 23:55:09 -0700

    tarea 26/02/2019

---
+++ index.js
@@ -0,0 +1,11 @@
+var fs=require('fs');
+var csv=require('fast-csv');
+
+fs.createReadStream('ejercicio04.csv')
+.pipe(csv())
+.on('data',function(data){
+    console.log(data);
+})
+.on('end',function(data){
+console.log('lectura finalizada');
+});
